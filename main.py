1a2b73d9c4470ae30b21dabc9d408c586e2ee721
ben-park <ben.park@thebytesize.ai>
2025-01-31 12:40:05 +0900

    chore: 페이지 문구 변경
    - 업로드 파일 형식 변경(xml -> xlsx)으로 인한 문구 변경

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from io import BytesIO
 from convert import convert
 
 st.title("출,퇴근 기록 파일 Converter")
-st.write("업로드한 XML 파일을 엑셀 파일로 변환한 후 다운로드할 수 있습니다.")
+st.write("업로드한 엑셀 파일의 양식을 변환한 후 다운로드할 수 있습니다.")
 
-uploaded_file = st.file_uploader("XML 파일을 업로드하세요", type=["xml"])
+uploaded_file = st.file_uploader("xlsx 파일을 업로드하세요", type=["xlsx"])
 
 if uploaded_file:
     try:
         # 임시 경로에 저장
-        with open("temp_file.xml", "wb") as temp_file:
+        with open("temp_file.xlsx", "wb") as temp_file:
             temp_file.write(uploaded_file.read())
         
         # XML을 Excel로 변환
-        st.write("📤 XML 파일을 처리 중입니다...")
-        workbook = convert("temp_file.xml")  # convert 함수 호출
+        st.write("📤 파일을 처리 중입니다...")
+        workbook = convert("temp_file.xlsx")  # convert 함수 호출
 
         # 처리 완료 메시지
         st.success("✅ 처리 완료되었습니다! 아래에서 엑셀 파일을 다운로드하세요.")
